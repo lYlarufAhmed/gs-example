69c64655f2534762c5fb40c093e6153c81ec61fb
pickboy900 <ahmedmaruf47@gmail.com>
2021-12-02 11:38:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {GoogleSpreadsheet} from "google-spreadsheet";
+import React from "react";
+import {Container, Row, Table} from "react-bootstrap";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [rows, setRows] = React.useState([])
+    const [headers, setHeaders] = React.useState([])
+
+    async function getRows() {
+        const doc = new GoogleSpreadsheet(process.env.REACT_APP_SPREADSHEET_ID);
+        // console.log('Hellow')
+        await doc.useServiceAccountAuth({
+            client_email: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY
+        })
+        await doc.loadInfo()
+        const sheet = doc.sheetsByIndex[0]
+        // console.log(sheet)
+        const rows = await sheet.getRows()
+        // console.log(rows)
+        setHeaders(sheet.headerValues)
+        setRows(rows)
+    }
+
+    React.useEffect(() => {
+        getRows()
+    }, [])
+    return (
+        <Container className="App">
+            <Row><h3>Keyword Research sheet</h3></Row>
+            <Row>
+                <Table striped bordered hover responsive size={'sm'}>
+
+                    <thead>
+                    <th>#</th>
+                    {headers.map(h => <th>{h}</th>)}
+                    </thead>
+                    <tbody>
+                    {rows.map(({Keyword, Volume}, i) => <tr>
+                        <td>{i + 1}</td>
+                        <td>{Keyword}</td>
+                        <td>{Volume}</td>
+                    </tr>)}
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td colSpan={2}/>
+                        <td>Total Volume: {rows.reduce((acc, {Volume}) => acc + parseInt(Volume, 10), 0)}</td>
+                    </tr>
+                    </tfoot>
+                </Table>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
